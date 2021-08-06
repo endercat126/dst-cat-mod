9ef0c1aa8c9be1f471bf1565de38f3d2417ca719
Endercat <84635059+endercat126@users.noreply.github.com>
2021-08-06 12:43:56 -0400

    update to 1.2.7

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 name = "Cat Character Mod"
 description = "haha cat go brrrr"
 author = "Endercat126, godzilly4"
-version = "1.2.6" -- This is the version of the template. Change it to your own number.
+version = "1.2.7" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
 forumthread = "/files/file/950-extended-sample-character/"
